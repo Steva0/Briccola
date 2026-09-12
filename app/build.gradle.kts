ce2a44f8098f35da46e8b5d4d6598df5ca59d082
Steva0 <michele.stevanin@gmail.com>
2026-09-12 15:20:18 +0200

    Bump version to 152 (v1.5.2) con pacchetto AAB/APK leggero (~24MB)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.briccola.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 151
-        versionName = "1.5.1"
+        versionCode = 152
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
